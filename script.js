922b8f7e5c26fc641f53b8fa7c1578f5b625ef36
Tomas David Restrepo Osorio <MaxterDuck@gmail.com>
2025-06-16 13:38:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+confirm("Deseas continuar a mi pagina?")
+console.log (confirm)
+
+// console.log ("Hello coder this is my test about Javascript")
+// console.warn("This is a error not so slight")
+// console.error("This error is about text for Javascript")
+// console.info("This message is about info")
+// console.group("This is my first group about Javascript")
+// console.log("Pepe 1")
+// console.log("Pepe 2")
+// console.log("Pepe 3")
+// console.groupEnd()
+// console.time('Time');
+// console.info('Testing')
+// console.timeEnd('Time');
+// prompt("Ingrese Su nombre", "Rana")
+// alert("Estas re pajin no? ")
+// confirm("Sos puto eh")
+// let hasCompletedModule = false;
+// console.log(hasCompletedModule)
